a4ad997d4908c81ce8a3fea949e9a2c16a1fc15d
lana <svetlasosnova@gmail.com>
2025-03-03 17:23:43 +0100

    Fixed clamp

--- Assets/Scripts/HorrorCore/CameraMan/CameraController.cs
+++ Assets/Scripts/HorrorCore/CameraMan/CameraController.cs
@@ -2,27 +2,39 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float yClamp = 30f;
-    
+
+    private bool gyroAvailable;
+    private float initYAngle;
     private float currentYAngle;
     
-#if UNITY_ANDROID
     void Start()
     {
-        Input.gyro.enabled = true;
+        if (SystemInfo.supportsGyroscope)
+        {
+            Input.gyro.enabled = true;
+            gyroAvailable = true;
+
+            initYAngle = transform.localEulerAngles.y;
+            currentYAngle = initYAngle;
+        }
+        else
+        {
+            Debug.LogWarning("Gyroscope not available on this device. Disabling rotation.");
+            gyroAvailable = false;
+        }
     }
 
     void FixedUpdate()
     {
-        Rotate();
+        if(gyroAvailable) Rotate();
     }
 
     private void Rotate()
     {
         float delta = -Input.gyro.rotationRateUnbiased.y * Mathf.Rad2Deg * Time.deltaTime;
-        currentYAngle += delta;
-        currentYAngle = Mathf.Clamp(currentYAngle, -yClamp, yClamp);
+        
+        currentYAngle = Mathf.Clamp(currentYAngle + delta, initYAngle-yClamp, initYAngle+yClamp);
 
         transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, currentYAngle, transform.localEulerAngles.z);
     }
-#endif
 }
