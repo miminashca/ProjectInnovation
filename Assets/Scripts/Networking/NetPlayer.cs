1d3a8386a67afb6e0631419d21fa699c99d6a6fe
lana <svetlasosnova@gmail.com>
2025-03-04 18:52:16 +0100

    movement working in layout scene

--- Assets/Scripts/Networking/NetPlayer.cs
+++ Assets/Scripts/Networking/NetPlayer.cs
@@ -9,10 +9,18 @@ public class NetPlayer : MonoBehaviour
         // If this Player object is not owned by me, disable its camera & listener
         Camera camera = GetComponentInChildren<Camera>();
         AudioListener listener = GetComponentInChildren<AudioListener>();
+        Canvas[] canvases = GetComponentsInChildren<Canvas>();
         if (!view.IsMine)
         {
             if (camera) camera.enabled = false;
             //if (listener) listener.enabled = false;
+            if (canvases.Length > 0)
+            {
+                foreach (Canvas canvas in canvases)
+                {
+                    canvas.gameObject.SetActive(false);
+                }
+            }
         }
     }
 }
