9cf8e7183ed3a2f1244b4d9f50b63549687c15eb
BoggyWoggy <523487@student.saxion.nl>
2025-03-11 11:00:53 +0100

    Working Music Slider with script

--- Assets/Prefabs/UI/UI Scripts/VolumeSettings.cs
+++ Assets/Prefabs/UI/UI Scripts/VolumeSettings.cs
@@ -9,5 +9,28 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private AudioMixer MusicMixer;
     [SerializeField] private Slider sfxSlider;
     [SerializeField] private Slider musicSlider;
-   
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicValue"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            setMusicVolume();
+        }
+       
+    }
+    public void setMusicVolume()
+    {
+        float musicVolume = musicSlider.value;
+        MusicMixer.SetFloat("MasterVolume", Mathf.Log10(musicVolume)*20);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume); //store the slider info inetween scenes and whatnot
+    }
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume"); //get the stored val
+        setMusicVolume();
+    }
 }
