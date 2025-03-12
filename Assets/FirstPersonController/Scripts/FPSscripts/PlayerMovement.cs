4de98520a01e600982997ffff06ea127fdeb51b6
lana <svetlasosnova@gmail.com>
2025-03-12 22:51:32 +0100

    Old input system + old player movement scripts

--- Assets/FirstPersonController/Scripts/FPSscripts/PlayerMovement.cs
+++ Assets/FirstPersonController/Scripts/FPSscripts/PlayerMovement.cs
@@ -81,10 +81,14 @@ public class PlayerMovement : MonoBehaviour
         Vector3 moveVector = Vector3.zero;
         if (joystick)
         {
-            moveVector += new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+            moveVector = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
         }
+        // Optionally, you can add a preprocessor check if you want to support keyboard input on other platforms.
 #if UNITY_EDITOR
-        moveVector += new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        else
+        {
+            moveVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        }
 #endif
 
         control = IsGrounded() ? ControlType.Velocity : ControlType.Force;
@@ -121,9 +125,10 @@ public class PlayerMovement : MonoBehaviour
         }
 
         // For mobile, you may want to disable the mouse input
-        float mouseX = 0;
 #if UNITY_EDITOR
-        mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
+#else
+    float mouseX = 0;
 #endif
 
         yRotation += (mouseX + touchVec.x);
