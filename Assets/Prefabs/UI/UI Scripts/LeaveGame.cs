8c1c7171efcfabf848268cec405bc82d9f59ea79
BoggyWoggy <523487@student.saxion.nl>
2025-03-13 14:42:44 +0100

    leavegamescript

--- Assets/Prefabs/UI/UI Scripts/LeaveGame.cs
+++ Assets/Prefabs/UI/UI Scripts/LeaveGame.cs
@@ -7,6 +7,7 @@ public class LeaveGame : MonoBehaviour
    public void QuitToMainMenu()
     {
         SceneManager.LoadScene(1); //1 is lobby scene
+        //haha uck me lol, oin ack to either lobby scene or loading scene makes the game unplayable
         //SceneManager.LoadScene("LobbyScene"); //accessing scene by name change the name if yall change the scene name
     }
 }
