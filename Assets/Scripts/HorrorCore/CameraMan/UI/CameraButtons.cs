719b4fba30f3028911b75f48ca4c0b22304d610e
sabrecool <simeon15dorne@gmail.com>
2025-03-13 12:43:22 +0100

    Re-implemented cameraman audio
    Reimplemented cam switch audio + CamAudioManager child object of CameraMan prefab and cameraman ambience

--- Assets/Scripts/HorrorCore/CameraMan/UI/CameraButtons.cs
+++ Assets/Scripts/HorrorCore/CameraMan/UI/CameraButtons.cs
@@ -1,8 +1,20 @@
 using UnityEngine;
 public class CameraButtons : MonoBehaviour
 {
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
     public void PressCameraButtonWithID(int ID)
     {
         EventBus.PressCameraSwitchButton(ID);
+
+        if (audioSource != null)
+        {
+            audioSource.Play();
+        }
     }
 }
