2aa45a96a0a8e22b73a157dd74b48dc3a0e07e2b
BoggyWoggy <523487@student.saxion.nl>
2025-03-13 19:50:59 +0100

    update to exit quest

--- Assets/Prefabs/UI/UI Scripts/UpdateExitQuest.cs
+++ Assets/Prefabs/UI/UI Scripts/UpdateExitQuest.cs
@@ -13,7 +13,7 @@ public class UpdateExitQuest : MonoBehaviour
     // Update is called once per frame
     public void EnableExit()
     {
-        if (GameManager.Instance.currentAmountOfPickups > 0)
+        if (GameManager.Instance.currentAmountOfPickups >= GameManager.Instance.minAmountOfPickups)
         {
             quest.SetActive(true);
         }
