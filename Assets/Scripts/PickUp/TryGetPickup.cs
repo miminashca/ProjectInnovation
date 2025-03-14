3fe1ac2680e5db3390204a236aa3c80752e0d649
lana <svetlasosnova@gmail.com>
2025-03-14 12:59:39 +0100

    Update TryGetPickup.cs

--- Assets/Scripts/PickUp/TryGetPickup.cs
+++ Assets/Scripts/PickUp/TryGetPickup.cs
@@ -53,27 +53,27 @@ public class TryGetPickup : MonoBehaviour
                 }
             }
         }
-// #if UNITY_EDITOR
-//         if (Input.GetMouseButtonDown(0)) // 0 = left mouse button or first touch
-//         {
-//             // ScreenPointToRay: from the main camera, create a ray from the tap/click position
-//             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-//             // RaycastHit is where the ray hits
-//             if (Physics.Raycast(ray, out RaycastHit hitInfo,(1<<LayerMask.NameToLayer("PlayerJar"))))
-//             {
-//                 //Debug.Log(hitInfo.transform);
-//                 // If the collider we hit has a "Pickup" script (or tag named "Pickup"):
-//                 // (Here, we assume you have a "Pickup" script or a Tag to identify pickups)
-//                 PickUp pickup = hitInfo.collider.GetComponent<PickUp>();
-//                 if (currentPickups.Contains(pickup))
-//                 {
-//                     EventBus.CollectPickup(pickup); // Your custom pickup handling
-//                     PickupUndetected(pickup);
-//                     Debug.Log("picked up");
-//                 }
-//             }
-//         }
-// #endif
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0)) // 0 = left mouse button or first touch
+        {
+            // ScreenPointToRay: from the main camera, create a ray from the tap/click position
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // RaycastHit is where the ray hits
+            if (Physics.Raycast(ray, out RaycastHit hitInfo,(1<<LayerMask.NameToLayer("PlayerJar"))))
+            {
+                //Debug.Log(hitInfo.transform);
+                // If the collider we hit has a "Pickup" script (or tag named "Pickup"):
+                // (Here, we assume you have a "Pickup" script or a Tag to identify pickups)
+                PickUp pickup = hitInfo.collider.GetComponent<PickUp>();
+                if (currentPickups.Contains(pickup))
+                {
+                    EventBus.CollectPickup(pickup); // Your custom pickup handling
+                    PickupUndetected(pickup);
+                    Debug.Log("picked up");
+                }
+            }
+        }
+#endif
         
     }
 
